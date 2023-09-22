ab14e7c1cc032a4d75d30d23bef0b194ec4e19cc
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-09-22 18:20:48 +0200

    Add changes icon Column js

--- react-project/src/components/Column/Column.js
+++ react-project/src/components/Column/Column.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import styles from './Column.module.scss';
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
 
-const Column = ({ title, icon }) => {
+const Column = (props) => {
   return (
-    <article className={styles.column}>
-      <h2 className={styles.title}>
-        <span className={`${styles.icon} fa fa-${icon}`} /> {title}
-      </h2>
-    </article>
+    <div className={styles.column}>
+      
+      <h2 className={styles.columnTitle}>{props.title} <span className={styles.icon + ' fa fa-' + props.icon} /></h2>
+      
+      <ul className={styles.cards}>
+        {props.cards.map((card) => (
+          <Card key={card.id} title={card.title} />
+        ))}
+      </ul>
+      <CardForm columnId={props.id} action={(newCard, columnId) => props.addCard(newCard, columnId)} />
+    </div>
   );
 };
 
