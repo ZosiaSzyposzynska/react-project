4ae6438b8c0246dae7f3a770cd82c0951a5589c8
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-09-22 18:21:58 +0200

    Add changes List js

--- react-project/src/components/List/List.js
+++ react-project/src/components/List/List.js
@@ -1,21 +1,81 @@
-import React from 'react';
-import Column from '../Column/Column';
+import React, { useState } from 'react';
 import styles from './List.module.scss';
+import Column from './../Column/Column';
+import ColumnForm from './../ColumnForm/ColumnForm';
+import CardForm from './../CardForm/CardForm'; 
+import shortid from 'shortid';
 
 const List = () => {
+  const [columns, setColumns] = useState([
+    {
+      id: 1,
+      title: 'Books',
+      icon: 'book',
+      cards: [
+        { id: 1, title: 'This is Going to Hurt' },
+        { id: 2, title: 'Interpreter of Maladies' },
+      ],
+    },
+    {
+      id: 2,
+      title: 'Movies',
+      icon: 'film',
+      cards: [
+        { id: 1, title: 'Harry Potter' },
+        { id: 2, title: 'Star Wars' },
+      ],
+    },
+    {
+      id: 3,
+      title: 'Games',
+      icon: 'gamepad',
+      cards: [
+        { id: 1, title: 'The Witcher' },
+        { id: 2, title: 'Skyrim' },
+      ],
+    },
+  ]);
+
+  const addColumn = (newColumn) => {
+    setColumns([
+      ...columns,
+      { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] },
+    ]);
+  };
+
+  const addCard = (newCard, columnId) => {
+    const columnsUpdated = columns.map((column) => {
+      if (column.id === columnId)
+        return { ...column, cards: [...column.cards, { id: shortid(), title: newCard.title }] };
+      else return column;
+    });
+
+    setColumns(columnsUpdated);
+  };
+
   return (
-    <div>
+    <div className={styles.list}>
       <header className={styles.header}>
         <h2 className={styles.title}>
-          Things to do<span className={styles.span}>soon</span>
+          Things to do<span>soon!</span>
         </h2>
-        <p className={styles.description}>Interesting things I want to check out</p>
       </header>
+      <p className={styles.description}>Interesting things I want to check out</p>
       <section className={styles.columns}>
-        <Column title="Books" icon="book" />
-        <Column title="Movies" icon="film" />
-        <Column title="Games" icon="gamepad" />
+        {columns.map((column) => (
+          <Column
+            key={column.id}
+            id={column.id}
+            title={column.title}
+            icon={column.icon}
+            cards={column.cards}
+            addCard={addCard}
+          />
+        ))}
       </section>
+
+      <ColumnForm action={addColumn} />
+      
     </div>
   );
 };
