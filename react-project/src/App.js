101f34d5c8680600c72c4d4e99732d23000fdc54
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-09-22 18:20:15 +0200

    Add App js

--- react-project/src/App.js
+++ react-project/src/App.js
@@ -3,13 +3,14 @@ import SearchForm from './components/SearchForm/SearchForm'
 import List from './components/List/List';
 import Container from './components/Container/Container';
 
+
 const App = () => {
   return (
     <Container>
       <Hero />
       <SearchForm />
       <List />
-
+      
     </Container>
   );
 };
