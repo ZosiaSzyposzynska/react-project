32161440150129f4735fa1baf38bd88f5651abe5
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-09-27 18:40:26 +0200

    Add handleSubmit and dispatch for update searchString action

--- react-project/src/components/SearchForm/SearchForm.js
+++ react-project/src/components/SearchForm/SearchForm.js
@@ -1,11 +1,27 @@
 import styles from './SearchForm.module.scss'
 import TextInput from '../TextInput/TextInput';
 import Button from '../Button/Button';
+import { useDispatch } from 'react-redux';
+import { useState } from 'react';
 
 const SearchForm = () => {
+    
+    const dispatch = useDispatch();
+
+    const [input, setInputValue] = useState('');
+
+    const handleSubmit = (e) => {
+
+        e.preventDefault();
+        dispatch({type: "UPDATE_SEARCHSTRING", payload: input });
+        setInputValue('');
+
+
+    }
+
     return (
-        <form className={styles.searchForm}>
-            <TextInput placeholder="Search…" />
+        <form className={styles.searchForm} onSubmit={handleSubmit}>
+            <TextInput placeholder="Search…" onChange={(e) => setInputValue(e.target.value)} />
             <Button>
                 <span className="fa fa-search" />
             </Button>
