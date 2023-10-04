f6447fbd552f9b686ea7fafcb0ba992e64aaed63
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-04 19:32:04 +0200

    Add Home/

---
+++ react-project/src/components/Home/Home.js
@@ -0,0 +1,21 @@
+import Hero from "../Hero/Hero";
+import SearchForm from "../SearchForm/SearchForm";
+import List from "../List/List";
+
+
+const Home = () => {
+
+    return (
+
+    <>
+      <Hero />
+      <SearchForm />
+      <List />
+    </>
+
+
+
+    );
+}
+
+export default Home;
