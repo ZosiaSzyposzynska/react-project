64248aa64657a63d14f11e96f84357c899ad1724
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-04 19:30:07 +0200

    Add About/

---
+++ react-project/src/components/About/About.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../PageTitle/PageTitle';
+
+const About = () => {
+  return (
+    <div>
+      <PageTitle>ABOUT</PageTitle> 
+      
+    </div>
+  );
+};
+
+export default About;
