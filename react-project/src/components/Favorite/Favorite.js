653a97369a8dd8e2fc594091728a8e7edd1e234d
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-04 19:32:30 +0200

    Add Favorite/

---
+++ react-project/src/components/Favorite/Favorite.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PageTitle from '../PageTitle/PageTitle';
+
+const Favorite = () => {
+  return (
+    <div>
+      <PageTitle>FAVORITE</PageTitle> 
+      
+    </div>
+  );
+};
+
+export default Favorite;
