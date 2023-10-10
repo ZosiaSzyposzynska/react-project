84fa67e38866f9fd4503b9753fe740dcf0ef0fd0
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-10 17:44:00 +0200

    Add changes Card

--- react-project/src/components/Card/Card.js
+++ react-project/src/components/Card/Card.js
@@ -1,10 +1,34 @@
 import React from 'react';
-import styles from './Card.module.scss';
+import { clsx } from 'clsx';
+import { useDispatch } from 'react-redux';
+import { toggleCardFavorite } from '../../redux/store';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar  } from '@fortawesome/free-solid-svg-icons';
+import 'font-awesome/css/font-awesome.min.css';
+import styles from './Card.module.scss'
 
-const Card = (props) => {
+const Card = ({ title, description, isFavorite, cardId }) => {
+  const dispatch = useDispatch();
+
+ const toggleFavorite = () => {
+  console.log('card id: ', cardId);
+  dispatch(toggleCardFavorite(cardId));
+};
+
+
   return (
-    <li className={styles.card}>{props.title}</li>
+    <div className={styles.card}>
+      <h3>{title}</h3>
+      <p>{description}</p>
+      <button
+        className={clsx('favorite-button', { 'is-favorite': isFavorite })}
+        onClick={toggleFavorite}
+      >
+        <FontAwesomeIcon icon={faStar} onClick={toggleFavorite} />
+      </button>
+    </div>
   );
 };
+
 
 export default Card;
