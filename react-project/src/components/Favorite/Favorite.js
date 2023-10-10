8ac689dd05b0b9f83a569a47cf90854c693ebb2c
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-10 17:43:05 +0200

    Add changes Favorite js

--- react-project/src/components/Favorite/Favorite.js
+++ react-project/src/components/Favorite/Favorite.js
@@ -1,11 +1,34 @@
+// Favorite.js
+
 import React from 'react';
-import PageTitle from '../PageTitle/PageTitle';
+import { useSelector } from 'react-redux';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { getFavoriteCards } from '../../redux/store';
+import styles from './Favorite.module.scss';
 
 const Favorite = () => {
+  const favoriteCards = useSelector((state) => getFavoriteCards(state));
+
   return (
-    <div>
-      <PageTitle>FAVORITE</PageTitle> 
-      
+    <div className={styles.favoriteColumn}>
+      <h2>FAVORITE</h2>
+      <div>
+        {favoriteCards.length === 0 ? (
+          <p>No favorite cards...</p>
+        ) : (
+          favoriteCards.map((card) => (
+            <div key={card.id} className={styles.favoriteCard} >
+              <h3>{card.title}</h3>
+              <p>{card.description}</p>
+              <FontAwesomeIcon
+                icon={faStar}
+                className={card.isFavorite ? styles['is-favorite'] : ''}
+              />
+            </div>
+          ))
+        )}
+      </div>
     </div>
   );
 };
