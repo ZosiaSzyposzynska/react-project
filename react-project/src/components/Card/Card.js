07a73294618b00814cf96d9c6e16067043bd2e49
zosiaszyp@gmail.com <zosiaszyp@gmail.com>
2023-10-13 11:11:13 +0200

    Add removeCard

--- react-project/src/components/Card/Card.js
+++ react-project/src/components/Card/Card.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { clsx } from 'clsx';
 import { useDispatch } from 'react-redux';
-import { toggleCardFavorite } from '../../redux/store';
+import { toggleCardFavorite } from '../../redux/cardsRedux';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faStar  } from '@fortawesome/free-solid-svg-icons';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
 import 'font-awesome/css/font-awesome.min.css';
-import styles from './Card.module.scss'
+import styles from './Card.module.scss';
+import { removeCard } from '../../redux/cardsRedux';
 
 const Card = ({ title, description, isFavorite, cardId }) => {
   const dispatch = useDispatch();
@@ -16,16 +18,33 @@ const Card = ({ title, description, isFavorite, cardId }) => {
 };
 
 
+const handleRemove = () => {
+
+
+  dispatch(removeCard(cardId));
+
+
+}
+
+
   return (
     <div className={styles.card}>
       <h3>{title}</h3>
       <p>{description}</p>
       <button
-        className={clsx('favorite-button', { 'is-favorite': isFavorite })}
+        className={clsx('favorite-button', isFavorite && styles.isFavorite )}
         onClick={toggleFavorite}
       >
-        <FontAwesomeIcon icon={faStar} onClick={toggleFavorite} />
+        <FontAwesomeIcon icon={faStar}  />
       </button>
+
+      <button
+        
+        onClick={handleRemove} 
+      >
+        <FontAwesomeIcon icon={faTrash} />
+      </button>
+
     </div>
   );
 };
